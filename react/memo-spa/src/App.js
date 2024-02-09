c48d99239ef6191590f74012fef3287840ab88bb
OdenTakashi <odentakashidesu@icloud.com>
2024-02-09 17:08:39 +0900

    Added edit form

--- react/memo-spa/src/App.js
+++ react/memo-spa/src/App.js
@@ -1,14 +1,19 @@
-import MemoList from './components/MemoList.js'
+import MemoList from './components/List.js'
+import { useState } from 'react';
 
 function App() {
   const memos = JSON.parse(localStorage.getItem("memos")) || []
+  const [isEditable, setIsEditable] = useState(true)
 
+  function saveContent() {
+    setIsEditable(!isEditable)
+  }
   return (
     <div className='h-screen'>
       <header className='ml-6 mt-2'>
         <p className='text-indigo-900 font-serif text-xl'>Memo App</p>
       </header>
-      <MemoList memoItems={memos}/>
+      <MemoList memoItems={memos} editable={isEditable} handleClick={saveContent}/>
     </div>
   );
 }
