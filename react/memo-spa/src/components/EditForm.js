02d9d194689639f944033023dc948619f2453ff1
OdenTakashi <odentakashidesu@icloud.com>
2024-03-26 10:22:18 +0900

    Add: add placeholder to textarea

--- react/memo-spa/src/components/EditForm.js
+++ react/memo-spa/src/components/EditForm.js
@@ -25,7 +25,7 @@ export default function EditForm({isEditable, memoLists, updateMemo, handleEditM
     return (
       <div className='text-sm w-1/2 m-auto mt-6'>
         <div className='mx-auto w-1/2'>
-          <textarea className='border' id='content' value={formContent} onChange={(e) => updateContent(e.target.value)}></textarea>
+          <textarea className='border' id='content' placeholder='TODO: shopping' value={formContent} onChange={(e) => updateContent(e.target.value)}></textarea>
           <div>
             <button className='border p-1' onClick={() => saveMemo(editingNumber)}>Save</button>
             <button className='border ml-3 p-1' onClick={() => handleEditMode('')}>Cancel</button>
