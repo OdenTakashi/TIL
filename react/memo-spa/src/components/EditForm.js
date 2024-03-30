4c9f1bb98d905ba4c7c64778634f9b992fc33b37
OdenTakashi <odentakashidesu@icloud.com>
2024-03-30 18:37:16 +0900

    Fix: Add indention to placeholder

--- react/memo-spa/src/components/EditForm.js
+++ react/memo-spa/src/components/EditForm.js
@@ -1,6 +1,6 @@
 export default function EditForm({isEditable, memoLists, updateMemo, handleEditMode, formContent, updateContent}) {
   const editingNumber = isEditable
-
+  const memoPlaceholder = 'TODO: Today Task \n- Running \n- Coding'
   function saveMemo(number) {
     let element = document.getElementById('content')
 
@@ -29,11 +29,11 @@ export default function EditForm({isEditable, memoLists, updateMemo, handleEditM
     return (
       <div className='text-sm w-1/2 m-auto mt-6'>
         <div className='mx-auto w-1/2'>
-          <textarea className='border' rows='10' cols='50' id='content' placeholder='TODO: shopping' value={formContent} onChange={(e) => updateContent(e.target.value)}></textarea>
+          <textarea className='border rounded p-2' rows='10' cols='50' id='content' placeholder={memoPlaceholder} value={formContent} onChange={(e) => updateContent(e.target.value)}></textarea>
           <div>
-            <button className='border p-1' onClick={() => saveMemo(editingNumber)}>Save</button>
-            <button className='border ml-3 p-1' onClick={() => handleEditMode('')}>Cancel</button>
-            <button className='border p-1 ml-3' onClick={() => deleteMemo()}>delete</button>
+            <button className='border p-1 rounded' onClick={() => saveMemo(editingNumber)}>Save</button>
+            <button className='border ml-3 p-1 rounded' onClick={() => handleEditMode('')}>Cancel</button>
+            <button className='border p-1 ml-3 rounded' onClick={() => deleteMemo()}>delete</button>
           </div>
         </div>
       </div>
