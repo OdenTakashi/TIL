af66a6ef2ebd18b23734a9a8ac3f3502139e58cb
OdenTakashi <odentakashidesu2@gmail.com>
2024-06-23 23:29:15 +0900

    Implement login-status-state using useContext

--- react/memo-spa/src/App.js
+++ react/memo-spa/src/App.js
@@ -2,11 +2,13 @@ import List from './components/List'
 import EditForm from './components/EditForm'
 import Header from './components/Header'
 import { useState, useEffect } from 'react'
+import { LoginContext } from './context/loginContext'
 
 function App() {
   const [memos, setMemos] = useState(JSON.parse(localStorage.getItem("memos")) || [])
   const [isEditable, setIsEditable] = useState('')
   const [context, setContext] = useState('')
+  const [loginStatus, setLoginStatus] = useState(false)
 
   useEffect(() => {
     localStorage.setItem('memos', JSON.stringify(memos))
@@ -36,10 +38,12 @@ function App() {
 
   return (
     <div className='h-screen'>
-      <Header/>
-     <div className='w-1/2 m-auto mt-3 text-xl text-zinc-700 font-semibold'>{runningMode()}</div>
-      <List memoItems={memos} handleClick={handleEditMode} handleFormContent={handleFormContent}/>
-      <EditForm updateMemo={updateMemo} isEditable={isEditable} memoLists={memos} handleEditMode={handleEditMode} formContent={context} updateContent={updateContent}/>
+      <LoginContext.Provider value={{loginStatus, setLoginStatus}}>
+        <Header/>
+        <div className='w-1/2 m-auto mt-3 text-xl text-zinc-700 font-semibold'>{runningMode()}</div>
+        <List memoItems={memos} handleClick={handleEditMode} handleFormContent={handleFormContent}/>
+        <EditForm updateMemo={updateMemo} isEditable={isEditable} memoLists={memos} handleEditMode={handleEditMode} formContent={context} updateContent={updateContent}/>
+      </LoginContext.Provider>
     </div>
   );
 }
