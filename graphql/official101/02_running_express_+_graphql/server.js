ff78c01657336dd150eda84595660766846b71e9
OdenTakashi <odentakashidesu2@gmail.com>
2024-09-08 16:52:51 +0900

    Delete unnecessary part

--- graphql/official101/02_running_express_+_graphql/server.js
+++ graphql/official101/02_running_express_+_graphql/server.js
@@ -1,4 +1,7 @@
-var { graphql, buildSchema } = require("graphql")
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+var { ruruHTML } = require("ruru/server")
 
 var schema = buildSchema(`
   type Query {
@@ -6,16 +9,26 @@ var schema = buildSchema(`
   }
 `)
 
-var rootValue = {
+var root = {
   hello() {
     return "Hello world!"
-  }
+  },
 }
 
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue
-}).then(response => {
-  console.log(response)
+var app = express()
+
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+)
+
+app.get("/", (_req, res) => {
+  res.type("html")
+  res.end(ruruHTML({ endpoint: "/graphql" }))
 })
+
+app.listen(4000)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
