a13e763c1900d545954083a1a75a8e201befd43a
OdenTakashi <odentakashidesu2@gmail.com>
2024-09-28 17:36:43 +0900

    Add til about object type

---
+++ graphql/official101/06_object_type/server.js
@@ -0,0 +1,57 @@
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+var { ruruHTML } = require("ruru/server")
+
+var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`)
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides)
+  }
+
+  roll({ numRolls }) {
+    var output =[]
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce())
+    }
+
+    return output
+  }
+}
+var root = {
+  getDie({ numSides }) {
+    return new RandomDie(numSides || 6)
+  }
+}
+
+var app = express()
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+)
+
+app.get("/", (_req, res) => {
+  res.type("html")
+  res.end(ruruHTML({ endpoint: "/graphql" }))
+})
+
+app.listen(4000)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
