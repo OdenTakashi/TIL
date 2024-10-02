f21dac0254c9cf97ba6482ffc5ee6c309ef62eef
OdenTakashi <odentakashidesu2@gmail.com>
2024-10-02 22:58:12 +0900

    Add middle table

--- rails/books/perfet_ROR/02_ror_and_mvc/book_admin/app/models/book.rb
+++ rails/books/perfet_ROR/02_ror_and_mvc/book_admin/app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about,  ->(theme) { where("name like ?", "%#{theme}%") }
   scope :find_price, ->(price) { where(find_by(price:)) }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
