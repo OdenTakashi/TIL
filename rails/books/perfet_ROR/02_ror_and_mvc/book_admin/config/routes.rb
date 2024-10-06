a1338fd42c2b842c1133f9506c0f18fb664b036e
OdenTakashi <odentakashidesu2@gmail.com>
2024-10-06 18:20:12 +0900

    Add user model in order to create admin

--- rails/books/perfet_ROR/02_ror_and_mvc/book_admin/config/routes.rb
+++ rails/books/perfet_ROR/02_ror_and_mvc/book_admin/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "profiles/show"
+  get "profiles/edit"
+  get "profiles/update"
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
