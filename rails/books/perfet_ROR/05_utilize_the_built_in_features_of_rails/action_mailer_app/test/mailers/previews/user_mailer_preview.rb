a2153294ebb1612fa563d1c609109af3c17377c5
naoki.kodama <odentakashidesu2@gmail.com>
2024-10-29 22:51:14 +0900

    Add welcome mail feature

--- rails/books/perfet_ROR/05_utilize_the_built_in_features_of_rails/action_mailer_app/test/mailers/previews/user_mailer_preview.rb
+++ rails/books/perfet_ROR/05_utilize_the_built_in_features_of_rails/action_mailer_app/test/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def welcome
+    UserMailer.with(to: "oden@example.com", name: "oden").welcome
+  end
 end
