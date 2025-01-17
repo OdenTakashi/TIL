17e0a965af7da0e053a1e6eefbda82258472fde6
naoki.kodama <odentakashidesu2@gmail.com>
2025-01-17 21:50:23 +0900

    Add cal method

--- ruby/oop/wc_command/wc/calculator.rb
+++ ruby/oop/wc_command/wc/calculator.rb
@@ -12,5 +12,23 @@ module Wc
     def count_bytes(file_content)
       file_content.bytesize
     end
+
+    def total_result(contents)
+      total = { lines: 0, words: 0, bytes: 0, file_name: 'total' }
+      contents.each do |hash|
+        total[:lines] += hash[:lines]
+        total[:words] += hash[:words]
+        total[:bytes] += hash[:bytes]
+      end
+      total
+    end
+    
+    def remove_words_bytes(contents)
+      contents.each do |content|
+        content.delete(:words)
+        content.delete(:bytes)
+      end
+      contents
+    end
   end
 end
