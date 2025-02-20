8fffc636f23bb051fecd41aab0c9c8b487b455ca
OdenTakashi <odentakashidesu2@gmail.com>
2025-02-20 20:13:32 +0900

    Linked the model to the database

--- rails/multi_db_demo_app/db/secondary_migrate/20250220110456_create_secondary_users.rb
+++ rails/multi_db_demo_app/db/secondary_migrate/20250220110456_create_secondary_users.rb
@@ -1,6 +1,7 @@
 class CreateSecondaryUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :secondary_users do |t|
+    create_table :users do |t|
+      t.integer :age
       t.timestamps
     end
   end
