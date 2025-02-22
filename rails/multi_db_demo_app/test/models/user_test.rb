e002d34ceb9911e55e7741f3de457a8a15fd3cd0
OdenTakashi <odentakashidesu2@gmail.com>
2025-02-22 10:36:01 +0900

    I have reproduced a possible issue in a multi-database environment.
    I have reproduced the following issue:
    When applying the include_version option and running a new migration on only one of the databases, the version of the primary database gets written.

--- rails/multi_db_demo_app/test/models/user_test.rb
+++ rails/multi_db_demo_app/test/models/user_test.rb
@@ -1,8 +1,10 @@
 # == Schema Information
+# Schema version: 20250301020209
 #
 # Table name: users
 #
 #  id         :integer          not null, primary key
+#  height     :integer
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
