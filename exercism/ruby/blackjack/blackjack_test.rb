26881c0a43f47f00765adf987b05d6d010ac553b
OdenTakashi <odentakashidesu2@gmail.com>
2025-07-01 23:41:07 +0900

    add exe blackjack

--- exercism/ruby/blackjack/blackjack_test.rb
+++ exercism/ruby/blackjack/blackjack_test.rb
@@ -98,131 +98,131 @@ class BlackjackTest < Minitest::Test
     assert_equal "blackjack", Blackjack.card_range("ace", "ten")
   end
 
-  # def test_first_turn_pair_of_aces
-  #   assert_equal "P", Blackjack.first_turn("ace", "ace", "ace")
-  # end
+  def test_first_turn_pair_of_aces
+    assert_equal "P", Blackjack.first_turn("ace", "ace", "ace")
+  end
 
-  # def test_first_turn_pair_of_jacks
-  #   assert_equal "S", Blackjack.first_turn("jack", "jack", "ace")
-  # end
+  def test_first_turn_pair_of_jacks
+    assert_equal "S", Blackjack.first_turn("jack", "jack", "ace")
+  end
 
-  # def test_first_turn_pair_of_queens
-  #   assert_equal "S", Blackjack.first_turn("queen", "queen", "ace")
-  # end
+  def test_first_turn_pair_of_queens
+    assert_equal "S", Blackjack.first_turn("queen", "queen", "ace")
+  end
 
-  # def test_first_turn_pair_of_twos
-  #   assert_equal "H", Blackjack.first_turn("two", "two", "ace")
-  # end
+  def test_first_turn_pair_of_twos
+    assert_equal "H", Blackjack.first_turn("two", "two", "ace")
+  end
 
-  # def test_first_turn_pair_of_five
-  #   assert_equal "H", Blackjack.first_turn("five", "five", "ace")
-  # end
+  def test_first_turn_pair_of_five
+    assert_equal "H", Blackjack.first_turn("five", "five", "ace")
+  end
 
-  # def test_first_turn_blackjack_with_ace_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("ace", "king", "ace")
-  # end
+  def test_first_turn_blackjack_with_ace_for_dealer
+    assert_equal "S", Blackjack.first_turn("ace", "king", "ace")
+  end
 
-  # def test_first_turn_blackjack_with_queen_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("jack", "king", "queen")
-  # end
+  def test_first_turn_blackjack_with_queen_for_dealer
+    assert_equal "S", Blackjack.first_turn("jack", "king", "queen")
+  end
 
-  # def test_first_turn_blackjack_with_five_for_dealer
-  #   assert_equal "W", Blackjack.first_turn("ace", "king", "five")
-  # end
+  def test_first_turn_blackjack_with_five_for_dealer
+    assert_equal "W", Blackjack.first_turn("ace", "king", "five")
+  end
 
-  # def test_first_turn_blackjack_with_nine_for_dealer
-  #   assert_equal "W", Blackjack.first_turn("ace", "king", "nine")
-  # end
+  def test_first_turn_blackjack_with_nine_for_dealer
+    assert_equal "W", Blackjack.first_turn("ace", "king", "nine")
+  end
 
-  # def test_first_turn_score_of_20
-  #   assert_equal "S", Blackjack.first_turn("king", "queen", "ace")
-  # end
+  def test_first_turn_score_of_20
+    assert_equal "S", Blackjack.first_turn("king", "queen", "ace")
+  end
 
-  # def test_first_turn_score_of_19
-  #   assert_equal "S", Blackjack.first_turn("king", "nine", "ace")
-  # end
+  def test_first_turn_score_of_19
+    assert_equal "S", Blackjack.first_turn("king", "nine", "ace")
+  end
 
-  # def test_first_turn_score_of_18
-  #   assert_equal "S", Blackjack.first_turn("queen", "eight", "ace")
-  # end
+  def test_first_turn_score_of_18
+    assert_equal "S", Blackjack.first_turn("queen", "eight", "ace")
+  end
 
-  # def test_first_turn_score_of_17
-  #   assert_equal "S", Blackjack.first_turn("jack", "seven", "ace")
-  # end
+  def test_first_turn_score_of_17
+    assert_equal "S", Blackjack.first_turn("jack", "seven", "ace")
+  end
 
-  # def test_first_turn_score_of_16_with_six_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("jack", "six", "six")
-  # end
+  def test_first_turn_score_of_16_with_six_for_dealer
+    assert_equal "S", Blackjack.first_turn("jack", "six", "six")
+  end
 
-  # def test_first_turn_score_of_16_with_seven_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("nine", "seven", "seven")
-  # end
+  def test_first_turn_score_of_16_with_seven_for_dealer
+    assert_equal "H", Blackjack.first_turn("nine", "seven", "seven")
+  end
 
-  # def test_first_turn_score_of_16_with_ace_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("nine", "seven", "ace")
-  # end
+  def test_first_turn_score_of_16_with_ace_for_dealer
+    assert_equal "H", Blackjack.first_turn("nine", "seven", "ace")
+  end
 
-  # def test_first_turn_score_of_15_with_six_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("nine", "six", "six")
-  # end
+  def test_first_turn_score_of_15_with_six_for_dealer
+    assert_equal "S", Blackjack.first_turn("nine", "six", "six")
+  end
 
-  # def test_first_turn_score_of_15_with_seven_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("eight", "seven", "seven")
-  # end
+  def test_first_turn_score_of_15_with_seven_for_dealer
+    assert_equal "H", Blackjack.first_turn("eight", "seven", "seven")
+  end
 
-  # def test_first_turn_score_of_15_with_queen_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("six", "nine", "queen")
-  # end
+  def test_first_turn_score_of_15_with_queen_for_dealer
+    assert_equal "H", Blackjack.first_turn("six", "nine", "queen")
+  end
 
-  # def test_first_turn_score_of_14_with_six_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("eight", "six", "six")
-  # end
+  def test_first_turn_score_of_14_with_six_for_dealer
+    assert_equal "S", Blackjack.first_turn("eight", "six", "six")
+  end
 
-  # def test_first_turn_score_of_14_with_seven_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("seven", "seven", "seven")
-  # end
+  def test_first_turn_score_of_14_with_seven_for_dealer
+    assert_equal "H", Blackjack.first_turn("seven", "seven", "seven")
+  end
 
-  # def test_first_turn_score_of_14_with_queen_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("five", "nine", "queen")
-  # end
+  def test_first_turn_score_of_14_with_queen_for_dealer
+    assert_equal "H", Blackjack.first_turn("five", "nine", "queen")
+  end
 
-  # def test_first_turn_score_of_13_with_six_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("seven", "six", "six")
-  # end
+  def test_first_turn_score_of_13_with_six_for_dealer
+    assert_equal "S", Blackjack.first_turn("seven", "six", "six")
+  end
 
-  # def test_first_turn_score_of_13_with_seven_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("six", "seven", "seven")
-  # end
+  def test_first_turn_score_of_13_with_seven_for_dealer
+    assert_equal "H", Blackjack.first_turn("six", "seven", "seven")
+  end
 
-  # def test_first_turn_score_of_13_with_queen_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("four", "nine", "queen")
-  # end
+  def test_first_turn_score_of_13_with_queen_for_dealer
+    assert_equal "H", Blackjack.first_turn("four", "nine", "queen")
+  end
 
-  # def test_first_turn_score_of_12_with_six_for_dealer
-  #   assert_equal "S", Blackjack.first_turn("six", "six", "six")
-  # end
+  def test_first_turn_score_of_12_with_six_for_dealer
+    assert_equal "S", Blackjack.first_turn("six", "six", "six")
+  end
 
-  # def test_first_turn_score_of_12_with_seven_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("five", "seven", "seven")
-  # end
+  def test_first_turn_score_of_12_with_seven_for_dealer
+    assert_equal "H", Blackjack.first_turn("five", "seven", "seven")
+  end
 
-  # def test_first_turn_score_of_12_with_queen_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("three", "nine", "queen")
-  # end
+  def test_first_turn_score_of_12_with_queen_for_dealer
+    assert_equal "H", Blackjack.first_turn("three", "nine", "queen")
+  end
 
-  # def test_first_turn_score_of_11_with_king_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("four", "seven", "king")
-  # end
+  def test_first_turn_score_of_11_with_king_for_dealer
+    assert_equal "H", Blackjack.first_turn("four", "seven", "king")
+  end
 
-  # def test_first_turn_score_of_10_with_two_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("four", "six", "two")
-  # end
+  def test_first_turn_score_of_10_with_two_for_dealer
+    assert_equal "H", Blackjack.first_turn("four", "six", "two")
+  end
 
-  # def test_first_turn_score_of_5_with_queen_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("two", "three", "queen")
-  # end
+  def test_first_turn_score_of_5_with_queen_for_dealer
+    assert_equal "H", Blackjack.first_turn("two", "three", "queen")
+  end
 
-  # def test_first_turn_score_of_4_with_five_for_dealer
-  #   assert_equal "H", Blackjack.first_turn("two", "two", "five")
-  # end
+  def test_first_turn_score_of_4_with_five_for_dealer
+    assert_equal "H", Blackjack.first_turn("two", "two", "five")
+  end
 end
