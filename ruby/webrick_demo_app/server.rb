ca845d0c63b51324009cfc95c7e62496a863a596
OdenTakashi <odentakashidesu2@gmail.com>
2026-04-03 12:04:13 +0900

    mv dir to newcomer_training_2026_kodama

--- ruby/webrick_demo_app/server.rb
+++
@@ -1,9 +0,0 @@
-require 'webrick'
-
-server = WEBrick::HTTPServer.new({
-  BindAddress:    'localhost',
-  Port:           3090,
-})
-
-server.mount('/', WEBrick::HTTPServlet::FileHandler, './html')
-server.start
